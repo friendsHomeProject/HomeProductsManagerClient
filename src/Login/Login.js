9acf0c2d14c5a3a71ca5f9a7f2af93d5772f1240
rivka kalmarski <rivkak@riscogroup.com>
2018-12-24 12:03:45 +0200

    css changes

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 
+import './Login.scss';
+
 export class Login extends Component {
     constructor(props) {
         super(props);
@@ -17,22 +19,28 @@ export class Login extends Component {
     login(event) {
         event.preventDefault();
         axios.post(`http://localhost:59904/api/user/login?name=${this.state.name}&password=${this.state.password}`)
-        .then(response => console.log(response));
+            .then(response => console.log(response));
     }
 
     render() {
         return (
-            <form onSubmit={this.login}>
-                <label>
-                    Name:
-                    <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
-                </label>
-                <label>
-                    Password:
-                    <input type="text" name="password" value={this.state.password} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
+            <div class="login">
+                <form onSubmit={this.login}>
+                    <label>
+                        Name:
+                        <br/>
+                        <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+                    </label>
+                    <br/>
+                    <label>
+                        Password:
+                        <br/>
+                        <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+                    </label>
+                    <br/>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
         );
     }
 }
